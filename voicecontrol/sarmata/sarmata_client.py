f21518310f73b7426ef88b2617f273fe40f438fc
Revzik <b.piekarz1997@gmail.com>
2019-01-08 20:19:07 +0100

    Fix import files names

--- voicecontrol/sarmata/sarmata_client.py
+++ voicecontrol/sarmata/sarmata_client.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from argparse import ArgumentParser
-from voicecontrol.sarmata.service.sarmata_settings import SarmataSettings
-from voicecontrol.sarmata.service.sarmata_recognize import SarmataRecognizer
-from voicecontrol.sarmata.service.sarmata_asr_pb2 import ResponseStatus, EMPTY, START_OF_INPUT
-from voicecontrol.sarmata.utils.audio_source import AudioStream
-from voicecontrol.sarmata.utils.mic_source import MicrophoneStream
+from sarmata.service.sarmata_settings import SarmataSettings
+from sarmata.service.sarmata_recognize import SarmataRecognizer
+from sarmata.service.sarmata_asr_pb2 import ResponseStatus, EMPTY, START_OF_INPUT
+from sarmata.utils.audio_source import AudioStream
+from sarmata.utils.mic_source import MicrophoneStream
 import sys
 from voicecontrol.sarmata.SARMATA_CLIENT_VERSION import SARMATA_CLIENT_VERSION
 
