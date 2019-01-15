0a051bdcc5d0980ee8878aa1e8f5bd847d73909f
Revzik <b.piekarz1997@gmail.com>
2019-01-16 00:44:50 +0100

    Add files

--- music_player_v2/command_handler.py
+++ music_player_v2/command_handler.py
@@ -1,7 +1,26 @@
-class CommandHandler:
+from threading import Thread
+import wx
+from wx.lib.newevent import NewEvent
+from ..voicecontrol.run_sarmata import runSarmata
 
-    def __init__(self, panel):
-        self.panel_ = panel
+EventVPlay, V_EVT_PLAY = NewEvent()
+EventVPause, V_EVT_PAUSE = NewEvent()
+EventVStop, V_EVT_STOP = NewEvent()
+EventVNext, V_EVT_NEXT = NewEvent()
+EventVPrev, V_EVT_PREV = NewEvent()
+EventVVolUp, V_EVT_VOL_UP = NewEvent()
+EventVVolDn, V_EVT_VOL_DN = NewEvent()
+
+
+class CommandHandlerThread(Thread):
+
+    def __init__(self, notify_window):
+        Thread.__init__(self)
+        self.notify_window = notify_window
+        self.start()
+
+    def recognize(self):
+        self.execute(runSarmata())
 
     def execute(self, command):
         if command == 'play':
@@ -10,26 +29,34 @@ class CommandHandler:
             self.pause()
         elif command == 'stop':
             self.stop()
+        elif command == 'next':
+            self.next()
+        elif command == 'prev':
+            self.prev()
         elif command == 'volumeUp':
             self.volumeUp()
         elif command == 'volumeDown':
             self.volumeDown()
 
     def play(self):
-        self.panel_.onPlay()
+        wx.PostEvent(self.notify_window, EventVPlay())
 
     def pause(self):
-        self.panel_.onPause()
+        wx.PostEvent(self.notify_window, EventVPause())
 
     def stop(self):
-        self.panel_.onStop()
+        wx.PostEvent(self.notify_window, EventVStop())
+
+    def next(self):
+        wx.PostEvent(self.notify_window, EventVNext())
+
+    def prev(self):
+        wx.PostEvent(self.notify_window, EventVPrev())
 
     def volumeUp(self):
-        self.panel_.currentVolume += 10
-        if self.panel_.currentVolume > 100:
-            self.panel_.currentVolume = 100
+        offset = 10
+        wx.PostEvent(self.notify_window, EventVVolUp(change=offset))
 
     def volumeDown(self):
-        self.panel_.currentVolume -= 10
-        if self.panel_.currentVolume < 0:
-            self.panel_.currentVolume = 0
+        offset = -10
+        wx.PostEvent(self.notify_window, EventVVolDn(change=offset))
--- music_player_v2/player_skeleton2.py
+++ music_player_v2/player_skeleton2.py
@@ -1,135 +1,149 @@
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 # player_skeleton2.py
 #
 # Created: 04/15/2010
 #
 # Author: Mike Driscoll - mike@pythonlibrary.org
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 import os
 import wx
 import wx.media
 import wx.lib.buttons as buttons
+from command_handler import *
 
 dirName = os.path.dirname(os.path.abspath(__file__))
 bitmapDir = os.path.join(dirName, 'bitmaps')
+
 
 ########################################################################
 class MediaPanel(wx.Panel):
     """"""
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def __init__(self, parent):
         """Constructor"""
         wx.Panel.__init__(self, parent=parent)
-        
+
         self.frame = parent
         self.currentVolume = 50
         self.createMenu()
         self.layoutControls()
-        
+
         sp = wx.StandardPaths.Get()
         self.currentFolder = sp.GetDocumentsDir()
         self.current_song = 0
         self.file_list = []
-        
+
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.onTimer)
         self.timer.Start(100)
-        
-    #----------------------------------------------------------------------
+
+        # Running voice recognition
+        self.commandHandler = CommandHandlerThread(self)
+
+        self.Bind(wx.EVT_CHAR_HOOK, self.onKeyPress)
+        self.Bind(V_EVT_PLAY, self.onVoicePlay)
+        self.Bind(V_EVT_PAUSE, self.onVoicePause)
+        self.Bind(V_EVT_STOP, self.onVoiceStop)
+        self.Bind(V_EVT_NEXT, self.onVoiceNext)
+        self.Bind(V_EVT_PREV, self.onVoicePrev)
+        self.Bind(V_EVT_VOL_UP, self.onVoiceVolumeUp)
+        self.Bind(V_EVT_VOL_DN, self.onVoiceVolumeDown)
+
+    # ----------------------------------------------------------------------
     def layoutControls(self):
         """
         Create and layout the widgets
         """
-        
+
         try:
             self.mediaPlayer = wx.media.MediaCtrl(self, style=wx.SIMPLE_BORDER)
         except NotImplementedError:
             self.Destroy()
             raise
-                
+
         # create playback slider
         self.playbackSlider = wx.Slider(self, size=wx.DefaultSize)
         self.Bind(wx.EVT_SLIDER, self.onSeek, self.playbackSlider)
-        
-        self.volumeCtrl = wx.Slider(self, style=wx.SL_VERTICAL|wx.SL_INVERSE)
+
+        self.volumeCtrl = wx.Slider(self, style=wx.SL_VERTICAL | wx.SL_INVERSE)
         self.volumeCtrl.SetRange(0, 100)
         self.volumeCtrl.SetValue(self.currentVolume)
         self.volumeCtrl.Bind(wx.EVT_SLIDER, self.onSetVolume)
-                
+
         # Create sizers
         mainSizer = wx.BoxSizer(wx.VERTICAL)
         hSizer = wx.BoxSizer(wx.HORIZONTAL)
         audioSizer = self.buildAudioBar()
-                
+
         # layout widgets
-        mainSizer.Add(self.playbackSlider, 1, wx.ALL|wx.EXPAND, 5)
-        hSizer.Add(audioSizer, 0, wx.ALL|wx.CENTER, 5)
+        mainSizer.Add(self.playbackSlider, 1, wx.ALL | wx.EXPAND, 5)
+        hSizer.Add(audioSizer, 0, wx.ALL | wx.CENTER, 5)
         hSizer.Add(self.volumeCtrl, 0, wx.ALL, 5)
         mainSizer.Add(hSizer)
-        
+
         self.SetSizer(mainSizer)
         self.Layout()
-        
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def buildAudioBar(self):
         """
         Builds the audio bar controls
         """
         audioBarSizer = wx.BoxSizer(wx.HORIZONTAL)
-        
-        self.buildBtn({'bitmap':'player_prev.png', 'handler':self.onPrev,
-                       'name':'prev'},
+
+        self.buildBtn({'bitmap': 'player_prev.png', 'handler': self.onPrev,
+                       'name': 'prev'},
                       audioBarSizer)
-        
+
         # create play/pause toggle button
         img = wx.Bitmap(os.path.join(bitmapDir, "player_play.png"))
         self.playPauseBtn = buttons.GenBitmapToggleButton(self, bitmap=img, name="play")
         self.playPauseBtn.Enable(False)
-        
+
         img = wx.Bitmap(os.path.join(bitmapDir, "player_pause.png"))
         self.playPauseBtn.SetBitmapSelected(img)
         self.playPauseBtn.SetInitialSize()
-        
+
         self.playPauseBtn.Bind(wx.EVT_BUTTON, self.onPlay)
         audioBarSizer.Add(self.playPauseBtn, 0, wx.LEFT, 3)
-        
-        btnData = [{'bitmap':'player_stop.png',
-                    'handler':self.onStop, 'name':'stop'},
-                    {'bitmap':'player_next.png',
-                     'handler':self.onNext, 'name':'next'}]
+
+        btnData = [{'bitmap': 'player_stop.png',
+                    'handler': self.onStop, 'name': 'stop'},
+                   {'bitmap': 'player_next.png',
+                    'handler': self.onNext, 'name': 'next'}]
         for btn in btnData:
             self.buildBtn(btn, audioBarSizer)
-            
+
         return audioBarSizer
-                    
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def buildBtn(self, btnDict, sizer):
         """"""
         bmp = btnDict['bitmap']
         handler = btnDict['handler']
-                
+
         img = wx.Bitmap(os.path.join(bitmapDir, bmp))
         btn = buttons.GenBitmapButton(self, bitmap=img, name=btnDict['name'])
         btn.SetInitialSize()
         btn.Bind(wx.EVT_BUTTON, handler)
         sizer.Add(btn, 0, wx.LEFT, 3)
-        
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def createMenu(self):
         """
         Creates a menu
         """
         menubar = wx.MenuBar()
-        
+
         fileMenu = wx.Menu()
         open_file_menu_item = fileMenu.Append(wx.NewId(), "&Open", "Open a File")
         menubar.Append(fileMenu, '&File')
         self.frame.SetMenuBar(menubar)
         self.frame.Bind(wx.EVT_MENU, self.onBrowse, open_file_menu_item)
-        
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def loadMusic(self, musicFile):
         """"""
         if not self.mediaPlayer.Load(musicFile):
@@ -141,47 +155,31 @@ class MediaPanel(wx.Panel):
             self.GetSizer().Layout()
             self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
             self.playPauseBtn.Enable(True)
-    
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def onBrowse(self, event):
         """
-        Opens file dialog to browse for music
+        Opens file dialog to browse for music folder
         """
-        """
-        wildcard = "MP3 (*.mp3)|*.mp3|"     \
-                   "WAV (*.wav)|*.wav"
-        dlg = wx.FileDialog(
-            self, message="Choose a file",
-            defaultDir=self.currentFolder, 
-            defaultFile="",
-            wildcard=wildcard,
-            style=wx.FD_OPEN | wx.FD_CHANGE_DIR
-            )
-        if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPath()
-            self.currentFolder = os.path.dirname(path)
-            self.loadMusic(path)
-        dlg.Destroy()
-        """
-        """ that should be changed so as we choose not a single file to play but whole directory and we create list (with help of os)"""
 
         ddg = wx.DirDialog(
             self, message="Choose a folder in which you have your music stored", defaultPath=self.currentFolder,
             style=wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST
-         )
+        )
 
         if ddg.ShowModal() == wx.ID_OK:
             self.currentFolder = ddg.GetPath()
-            for fname in os.listdir(self.currentFolder):  # os.listdir returns the list of files in the currentFolder given in brackets
+            for fname in os.listdir(
+                    self.currentFolder):  # os.listdir returns the list of files in the currentFolder given in brackets
                 if fname.endswith('.mp3') or fname.endswith('.wav'):
                     self.file_list.append(fname)
             self.loadMusic(os.path.join(self.currentFolder, self.file_list[self.current_song]))
         ddg.Destroy()
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def onNext(self, event):
         """
-        Not implemented!
+        Switch to the next song in a folder (needs to be manually restarted)
         """
         self.current_song = self.current_song + 1
         if self.current_song >= len(self.file_list):
@@ -198,16 +196,14 @@ class MediaPanel(wx.Panel):
             self.GetSizer().Layout()
             self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
 
-        event.Skip()
-
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def onPause(self):
         """
         Pauses the music
         """
         self.mediaPlayer.Pause()
-    
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def onPlay(self, event):
         """
         Plays the music
@@ -215,7 +211,7 @@ class MediaPanel(wx.Panel):
         if not event.GetIsDown():
             self.onPause()
             return
-        
+
         if not self.mediaPlayer.Play():
             wx.MessageBox("Unable to Play media : Unsupported format?",
                           "ERROR",
@@ -224,13 +220,13 @@ class MediaPanel(wx.Panel):
             self.mediaPlayer.SetInitialSize()
             self.GetSizer().Layout()
             self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
-            
+
         event.Skip()
-    
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def onPrev(self, event):
         """
-        Not implemented!
+        Switch to previous song in a folder (needs to be manually restarted)
         """
         self.current_song = self.current_song - 1
         if self.current_song < 0:
@@ -247,9 +243,7 @@ class MediaPanel(wx.Panel):
             self.GetSizer().Layout()
             self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
 
-        event.Skip()
-
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def onSeek(self, event):
         """
         Seeks the media file according to the amount the slider has
@@ -257,25 +251,24 @@ class MediaPanel(wx.Panel):
         """
         offset = self.playbackSlider.GetValue()
         self.mediaPlayer.Seek(offset)
-        
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def onSetVolume(self, event):
         """
         Sets the volume of the music player
         """
-        self.currentVolume = self.volumeCtrl.GetValue()/100
-        # print("setting volume to: %s" % int(self.currentVolume*100))
-        self.mediaPlayer.SetVolume(self.currentVolume)
-    
-    #----------------------------------------------------------------------
+        self.currentVolume = self.volumeCtrl.GetValue()
+        self.mediaPlayer.SetVolume(self.currentVolume / 100)
+
+    # ----------------------------------------------------------------------
     def onStop(self, event):
         """
         Stops the music and resets the play button
         """
         self.mediaPlayer.Stop()
         self.playPauseBtn.SetToggle(False)
-        
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def onTimer(self, event):
         """
         Keeps the player slider updated
@@ -283,15 +276,115 @@ class MediaPanel(wx.Panel):
         offset = self.mediaPlayer.Tell()
         self.playbackSlider.SetValue(offset)
 
+    # ----------------------Voice controll handlers-------------------------
+    def onKeyPress(self, event):
+        """
+        Run voice recognition when spacebar is pressed
+        """
+        keycode = event.GetKeyCode()
+        if keycode == wx.WXK_SPACE:
+            self.commandHandler.recognize()
+
+        event.Skip()
+
+    # ----------------------------------------------------------------------
+    def onVoicePlay(self, event):
+        """
+        Play the song when command is received
+        """
+        if not self.mediaPlayer.Play():
+            wx.MessageBox("Unable to Play media : Unsupported format?",
+                          "ERROR",
+                          wx.ICON_ERROR | wx.OK)
+        else:
+            self.mediaPlayer.SetInitialSize()
+            self.GetSizer().Layout()
+            self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
+            self.playPauseBtn.SetToggle(True)
+
+    # ----------------------------------------------------------------------
+    def onVoicePause(self, event):
+        """
+        Pause current song when command is received
+        """
+        self.mediaPlayer.Pause()
+        self.playPauseBtn.SetToggle(False)
+
+    # ----------------------------------------------------------------------
+    def onVoiceStop(self, event):
+        """
+        Stop current song when command is received
+        """
+        self.mediaPlayer.Stop()
+        self.playPauseBtn.SetToggle(False)
+
+    # ----------------------------------------------------------------------
+    def onVoiceNext(self, event):
+        """
+        Switch to the next song in a folder when command is received (needs to be manually restarted)
+        """
+        self.current_song = self.current_song + 1
+        if self.current_song >= len(self.file_list):
+            self.current_song = 0
+
+        self.mediaPlayer.Stop()
+        self.loadMusic(os.path.join(self.currentFolder, self.file_list[self.current_song]))
+        if not self.mediaPlayer.Play():
+            wx.MessageBox("Unable to Play media : Unsupported format?",
+                          "ERROR",
+                          wx.ICON_ERROR | wx.OK)
+        else:
+            self.mediaPlayer.SetInitialSize()
+            self.GetSizer().Layout()
+            self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
+
+    # ----------------------------------------------------------------------
+    def onVoicePrev(self, event):
+        """
+        Switch to previous song in a folder when command is received (needs to be manually restarted)
+        """
+        self.current_song = self.current_song - 1
+        if self.current_song < 0:
+            self.current_song = len(self.file_list) - 1
+
+        self.mediaPlayer.Stop()
+        self.loadMusic(os.path.join(self.currentFolder, self.file_list[self.current_song]))
+        if not self.mediaPlayer.Play():
+            wx.MessageBox("Unable to Play media : Unsupported format?",
+                          "ERROR",
+                          wx.ICON_ERROR | wx.OK)
+        else:
+            self.mediaPlayer.SetInitialSize()
+            self.GetSizer().Layout()
+            self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
+
+    # ----------------------------------------------------------------------
+    def onVoiceVolumeUp(self, event):
+        self.currentVolume += event.change
+        if self.currentVolume > 100:
+            self.currentVolume = 100
+        self.mediaPlayer.SetVolume(self.currentVolume / 100)
+        self.volumeCtrl.SetValue(self.currentVolume)
+
+    # ----------------------------------------------------------------------
+    def onVoiceVolumeDown(self, event):
+        self.currentVolume -= event.change
+        if self.currentVolume < 0:
+            self.currentVolume = 0
+        self.mediaPlayer.SetVolume(self.currentVolume / 100)
+        self.volumeCtrl.SetValue(self.currentVolume)
+
+
 ########################################################################
 class MediaFrame(wx.Frame):
- 
-    #----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
     def __init__(self):
         wx.Frame.__init__(self, None, wx.ID_ANY, "Python Music Player")
         panel = MediaPanel(self)
-        
-#----------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------
 # Run the program
 if __name__ == "__main__":
     app = wx.App(False)
